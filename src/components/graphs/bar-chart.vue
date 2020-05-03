92f40992a9b0c480653b400e71fdaab3f17d172b
andreas.dennerlein <andreas.dennerlein@arsmedium.com>
2020-05-03 22:33:51 +0200

    feat(components): added first graph components

---
+++ src/components/graphs/bar-chart.vue
@@ -0,0 +1,62 @@
+<script>
+
+import {Bar, mixins} from 'vue-chartjs'
+
+const {reactiveProp} = mixins
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  watch: {
+    chartData: function (newVal, oldVal) {
+      console.log(this.options)
+      this.addGradient()
+      this.renderChart(this.chartData, this.options)
+    }
+  },
+  methods: {
+    hexToRgb: function (hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null
+    },
+    createGradient (colors) {
+      let gradientArr = []
+      colors.map(color => {
+        let gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+        gradient.addColorStop(0, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 1 + ')')
+        gradient.addColorStop(0.5, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 0.5 + ')')
+        gradient.addColorStop(1, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 0 + ')')
+        gradientArr.push(gradient)
+      })
+      return gradientArr
+    },
+    addGradient: function () {
+      this.chartData.datasets.map(dataset => {
+        dataset.backgroundColor = this.createGradient(dataset.backgroundColor)
+      })
+    }
+  },
+  mounted () {
+    this.addGradient()
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/graphs/line-chart.vue
@@ -0,0 +1,54 @@
+<script>
+
+import {Line, mixins} from 'vue-chartjs'
+import trendline from 'chartjs-plugin-trendline'
+
+const {reactiveProp} = mixins
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  components: {
+    trendline
+  },
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    hexToRgb: function (hex) {
+      // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null
+    },
+    createGradient (color) {
+      // create gradient with more datasets
+      let gradient = this.$refs.canvas.getContext('2d')
+      let gradientFIll = gradient.createLinearGradient(0, 0, 0, 450)
+      gradientFIll.addColorStop(0, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 0.5 + ')')
+      gradientFIll.addColorStop(0.5, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 0.25 + ')')
+      gradientFIll.addColorStop(1, 'rgba(' + this.hexToRgb(color).r + ',' + this.hexToRgb(color).g + ',' + this.hexToRgb(color).b + ',' + 0 + ')')
+      return gradientFIll
+    },
+    addGradient: function () {
+      this.chartData.datasets.map(dataset => {
+        dataset.backgroundColor = this.createGradient(dataset.backgroundColor)
+      })
+    }
+  },
+  mounted () {
+    this.addGradient()
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
