b9fc2702d3d92b2aa31adf71c9d6719a86f33b6e
andreas.dennerlein <andreas.dennerlein@arsmedium.com>
2020-05-24 16:06:49 +0200

    feat: added dummy content for profile route

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>Profile</div>
+  <div class="profile">
+    <section class="section section-tournament">
+      <description>
+        <h2 slot="headline">Profil</h2>
+        <p slot="text" class="text">
+          Here could be a profil an player only statistics.
+        </p>
+      </description>
+    </section>
+  </div>
 </template>
 
 <script>
+import Description from './../components/description-block'
+
 export default {
-  name: 'PlayerComparison'
+  name: 'PlayerComparison',
+  components: {
+    Description
+  }
 }
 </script>
 
